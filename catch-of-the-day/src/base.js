89f649dcf5dcc120ce9b46f25b6be6c15406f112
Brian Weaver <brian.weaver@northpoint.org>
2017-05-10 09:20:36 -0400

    Initial commit

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyD91AixznFCBtfXb2uOSLuAkBNjlhVRMdk",
+  authDomain: "catch-of-the-day-bweave-fd528.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-bweave-fd528.firebaseio.com",
+});
+
+export default base;
